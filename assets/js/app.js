d16b874f4c6fe33317a655dbc480c8d0e7440b8f
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2024-12-04 15:42:23 +0530

    back to top

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", () => {
       const content = header.nextElementSibling;
       const iconWrapper = header.querySelector(".icon-wrapper");
 
-      // Close other accordions
       document.querySelectorAll(".accordion-content").forEach((otherContent) => {
         if (otherContent !== content) {
           otherContent.classList.add("hidden");
@@ -18,8 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
           otherIconWrapper.classList.remove("active");
         }
       });
-
-      // Toggle current accordion
       const isHidden = content.classList.contains("hidden");
       content.classList.toggle("hidden");
       content.style.maxHeight = isHidden ? content.scrollHeight + "px" : null;
@@ -30,3 +27,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // ==============current year
 document.getElementById("current-year").textContent = new Date().getFullYear();
+
+// =============back to top===========
+
+const backTop = document.querySelector(".back-top");
+
+backTop.onclick = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+window.onscroll = () => {
+  if (window.scrollY > 150) {
+    backTop.classList.remove("hidden");
+    backTop.classList.add("block");
+  } else {
+    backTop.classList.remove("block");
+    backTop.classList.add("hidden");
+  }
+};
